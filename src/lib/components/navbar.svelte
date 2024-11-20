b80cceaad238ecfc2a13261c7934a783c3cde0ba
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-11-20 20:15:51 +0100

    fix nav links

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -2,6 +2,7 @@
 	import AppLogo from '$lib/components/appLogo.svelte';
 	import settings from '$lib/site/settings.json';
 	import { page } from '$app/stores';
+	import { base } from '$app/paths';
 </script>
 
 <header class="bg-white">
@@ -12,11 +13,12 @@
 		</div>
 		<div class="flex gap-x-8">
 			{#each settings.menu as item}
+				{@const href = base + item.href}
 				<a
-					href={item.href}
+					{href}
 					class="text-sm font-semibold leading-6 transition-colors"
-					class:text-gray-900={$page.url.pathname !== item.href}
-					class:text-blue-600={$page.url.pathname === item.href}
+					class:text-gray-900={$page.url.pathname !== href}
+					class:text-blue-600={$page.url.pathname === href}
 				>
 					{item.label}
 				</a>
