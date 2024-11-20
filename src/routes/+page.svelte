bd1c03eefdc3baa4d2750af33f8b6eeb2e808992
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-11-20 20:13:18 +0100

    test push

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,12 @@
 	import HeroPage from '$lib/components/heroSection.svelte';
 	import Introduction from '$lib/components/introduction.svelte';
 	import Features from '$lib/components/features.svelte';
+	import { base } from '$app/paths';
+
+	console.log(base);
 </script>
 
 <HeroPage />
+<pre>{JSON.stringify(base)}</pre>
 <Introduction />
 <Features />
