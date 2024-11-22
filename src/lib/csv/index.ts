edb3fc12930a3afb2e3b252d96652baf4d6cf1c4
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-11-22 17:50:48 +0100

    fix action

--- src/lib/csv/index.ts
+++ src/lib/csv/index.ts
@@ -16,7 +16,7 @@ async function main() {
 	for (const content of collections) {
 		await generateCollectionCsv(content);
 	}
-	console.log(`✨ CSV files generated at: ${import.meta.dirname} ✨`);
+	console.log(`✨ CSV files generated ✨`);
 }
 
 async function generateCollectionCsv(contentModel: ContentModel) {
