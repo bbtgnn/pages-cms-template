a8022563bb1ec1d7dba9b664bcb67918e7aec13b
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-11-25 15:52:18 +0100

    improved data route

--- src/routes/data/+page.server.ts
+++ src/routes/data/+page.server.ts
@@ -4,14 +4,14 @@ import { pipe, Record, Array } from 'effect';
 export const prerender = true;
 
 export const load = async () => {
-	const csvFileLoaders = import.meta.glob('$lib/csv/*.csv', { query: '?raw' });
+	const dataFileLoaders = import.meta.glob('$lib/export-data/*.{csv,json}', { query: '?raw' });
 
-	const csvFilePaths = pipe(
-		Record.toEntries(csvFileLoaders),
+	const dataFilePaths = pipe(
+		Record.toEntries(dataFileLoaders),
 		Array.map(([path]) => GITHUB_RAW_URL + path)
 	);
 
 	return {
-		csvFilePaths
+		dataFilePaths
 	};
 };
--- src/routes/data/+page.svelte
+++ src/routes/data/+page.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <div class="mx-auto max-w-7xl space-y-1 p-8">
-	{#each data.csvFilePaths as path}
+	{#each data.dataFilePaths as path}
 		<a href={path} class="block text-base-content underline hover:no-underline" target="_blank">
 			{path}
 		</a>
